e8458bffd09b479dc9d50284105339fc14436169
shubham <shubham.ingole@neosofttech.com>
2020-12-29 15:09:32 +0530

    maim

--- App.js
+++ App.js
@@ -1,21 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View,Button,TextInput, ScrollView,FlatList } from 'react-native';
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
 
 export default function App() {
+  const [taskList, setTaskList] = useState([]);
+
+  const addGoalHandler = goalTitle => {
+    setTaskList(currentTasks => [
+      ...currentTasks, {id: Math.random().toString(),
+         value : goalTitle}
+        ]);
+  };
+
+  const removeGoalHandler = taskId => {
+    setTaskList(currentTasks => {
+      return currentTasks.filter((task) => taskId !== task.id );
+    })
+  };
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.screen}>
+
+      <GoalInput onAddGoal={addGoalHandler} />
+      
+      <FlatList
+       keyExtractor={(item, index) => item.id}
+       data={taskList} renderItem={itemData => (
+        <GoalItem
+         id={itemData.item.id}
+         onDelete={removeGoalHandler} 
+         title={itemData.item.value}
+        />  
+      )}
+       />
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50
   },
+  
+  
 });
